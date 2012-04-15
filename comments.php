80aa2789206b49efeeca6872eab8479feab808f1
Nate Bird <natebird@gmail.com>
2012-04-15 19:24:37 -0400

    SMM theme 1.0
    This is initial theme for Simply Modern Mom

---
+++ comments.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * The template for displaying Comments.
+ *
+ * The area of the page that contains both current comments
+ * and the comment form.  The actual display of comments is
+ * handled by a callback to twentyten_comment which is
+ * located in the functions.php file.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+?>
+
+			<div id="comments">
+<?php if ( post_password_required() ) : ?>
+				<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyten' ); ?></p>
+			</div><!-- #comments -->
+<?php
+		/* Stop the rest of comments.php from being processed,
+		 * but don't kill the script entirely -- we still have
+		 * to fully load the template.
+		 */
+		return;
+	endif;
+?>
+
+<?php
+	// You can start editing here -- including this comment!
+?>
+
+<?php if ( have_comments() ) : ?>
+			<h3 id="comments-title"><?php
+			printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'twentyten' ),
+			number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
+			?></h3>
+
+<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+			<div class="navigation">
+				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+			</div> <!-- .navigation -->
+<?php endif; // check for comment navigation ?>
+
+			<ol class="commentlist">
+				<?php
+					/* Loop through and list the comments. Tell wp_list_comments()
+					 * to use twentyten_comment() to format the comments.
+					 * If you want to overload this in a child theme then you can
+					 * define twentyten_comment() and that will be used instead.
+					 * See twentyten_comment() in twentyten/functions.php for more.
+					 */
+					wp_list_comments( array( 'callback' => 'twentyten_comment' ) );
+				?>
+			</ol>
+
+<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+			<div class="navigation">
+				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+			</div><!-- .navigation -->
+<?php endif; // check for comment navigation ?>
+
+<?php else : // or, if we don't have comments:
+
+	/* If there are no comments and comments are closed,
+	 * let's leave a little note, shall we?
+	 */
+	if ( ! comments_open() ) :
+?>
+	<p class="nocomments"><?php _e( 'Comments are closed.', 'twentyten' ); ?></p>
+<?php endif; // end ! comments_open() ?>
+
+<?php endif; // end have_comments() ?>
+
+  <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="respond">
+  <?php cancel_comment_reply_link() ?>
+  	<textarea name="comment" id="comment" rows="10" tabindex="4"></textarea>
+  
+  	<div class="rules">What's on your mind? Tell me. All comments are screened for appropriateness. Good comments will be cherished, bad comments will be deleted.</div>
+  	
+  	<div class="form group">	
+  	
+  	<?php if ( $user_ID ) : ?>
+  		<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Log out &raquo;</a></p>
+  	<?php else : ?>
+  
+  		<p><label for="author"><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+  			<small>Name</small> <?php if ($req) echo "<span>(required)</span>"; ?></label></p>
+  
+  		<p><label for="email"><input type="email" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+  			<small>Email</small> <span>(<?php if ($req) echo "required, "; ?>not published)</span></label></p>
+  
+  		<p><label for="url"><input type="url" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
+  			<small>Website</small> <span>(not required)</span></label></p>
+  
+  <?php endif; ?>
+  
+  	<p><input name="submit" type="submit" id="submit" tabindex="5" value="submit" />
+  		<?php comment_id_fields(); ?>
+  	</p>
+  		<?php do_action('comment_form', $post->ID); ?>
+  	</div>
+  </form>
+
+</div><!-- #comments -->
